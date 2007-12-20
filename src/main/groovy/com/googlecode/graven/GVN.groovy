9f2e42ed4799c16f0504060b3ff8c48d963ff7b3
spullara <spullara@1ddc80c7-8841-0410-a5a5-81f158d74672>
2007-12-20 22:28:08 +0000

    completely self hosting. execute groovy src/main/groovy/com/googlecode/graven/GVN.groovy install

--- src/main/groovy/com/googlecode/graven/GVN.groovy
+++ src/main/groovy/com/googlecode/graven/GVN.groovy
@@ -4,9 +4,9 @@ package com.googlecode.graven;
 import groovy.xml.MarkupBuilder
 
 GroovyShell shell = new GroovyShell()
-String filename = args.length == 0 ? "pom.groovy" : args[0]
-Script script = shell.parse(new File(filename))
-MarkupBuilder mb = new MarkupBuilder()
+Script script = shell.parse(new File("pom.groovy"))
+def fw = new FileWriter("pom.xml");
+MarkupBuilder mb = new MarkupBuilder(fw)
 def binding = new Binding()
 binding.setProperty "project", { pom ->
     mb.project {
@@ -21,9 +21,14 @@ def includeClass = {clazz ->
     include.binding.properties.variables.each {
         binding.setVariable it.key, it.value.curry(mb)
     }
-    println binding.properties.variables
 }
 includeClass(Pom)
 binding.setProperty "include", includeClass
 script.binding = binding
 script.run();
+fw.close();
+def list = ["mvn"]
+list.addAll(args as List)
+new ProcessBuilder(list).redirectErrorStream(true).start().in.eachLine {
+    println it
+}

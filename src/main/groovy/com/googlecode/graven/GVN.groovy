0eb5fbe5d609ca1cad3ac5e2f58fcf67be66a6ad
spullara <spullara@1ddc80c7-8841-0410-a5a5-81f158d74672>
2007-12-20 23:17:17 +0000

    lets comment some code

--- src/main/groovy/com/googlecode/graven/GVN.groovy
+++ src/main/groovy/com/googlecode/graven/GVN.groovy
@@ -3,10 +3,17 @@ package com.googlecode.graven;
 
 import groovy.xml.MarkupBuilder
 
+// Read the pom.groovy script
 GroovyShell shell = new GroovyShell()
 Script script = shell.parse(new File("pom.groovy"))
+
+// Write out a pom.xml
 def fw = new FileWriter("pom.xml");
+
+// Setup the markup builder
 MarkupBuilder mb = new MarkupBuilder(fw)
+
+// Bind the markup builder into the script
 def binding = new Binding()
 binding.setProperty "project", { pom ->
     mb.project {
@@ -15,6 +22,8 @@ binding.setProperty "project", { pom ->
         pom()
     }
 }
+
+// Include all of the variables on the included script in our binding
 def includeClass = {clazz ->
     def include = clazz.newInstance()
     include.run()
@@ -22,11 +31,19 @@ def includeClass = {clazz ->
         binding.setVariable it.key, it.value.curry(mb)
     }
 }
+
+// Standard library
 includeClass(Pom)
+
+// Add the includer to the binding
 binding.setProperty "include", includeClass
+
+// Set the script binding and execute
 script.binding = binding
 script.run();
 fw.close();
+
+// Excecute maven 2
 def list = ["mvn"]
 list.addAll(args as List)
 new ProcessBuilder(list).redirectErrorStream(true).start().in.eachLine {

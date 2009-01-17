af1b97ab5c91c16171b24b23211109a5ce9ab598
spullara <spullara@1ddc80c7-8841-0410-a5a5-81f158d74672>
2009-01-17 01:36:18 +0000

    add some new things to the pom and make some old ones better

--- src/main/groovy/com/googlecode/graven/Pom.groovy
+++ src/main/groovy/com/googlecode/graven/Pom.groovy
@@ -9,11 +9,12 @@ testdependency = {dependencies, gid, aid, v ->
     }
 }
 
-dependency = {dependencies, gid, aid, v ->
+dependency = {dependencies, gid, aid, v, s = "compile" ->
     dependencies.dependency {
         groupId gid
         artifactId aid
         version v
+        scope s
     }
 }
 
@@ -61,33 +62,33 @@ repository = {parent, _id, _name, _url ->
     }
 }
 
-groovydeps = { dependencies ->
-	dependencies.dependency {
-		groupId "org.codehaus.groovy.maven"
-		artifactId "gmaven-mojo"
-		version "1.0-rc-2"
-	}
-	dependencies.dependency {
-		groupId "org.codehaus.groovy.maven.runtime"
-		artifactId "gmaven-runtime-default"
-		version "1.0-rc-2"
-	}
+groovydeps = {dependencies ->
+    dependencies.dependency {
+        groupId "org.codehaus.groovy.maven"
+        artifactId "gmaven-mojo"
+        version "1.0-rc-2"
+    }
+    dependencies.dependency {
+        groupId "org.codehaus.groovy.maven.runtime"
+        artifactId "gmaven-runtime-default"
+        version "1.0-rc-2"
+    }
 }
 
-groovy = { plugins -> 
-	plugins.plugin {
-		groupId "org.codehaus.groovy.maven"
-		artifactId "gmaven-plugin"
-		version "1.0-rc-2"
-		executions {
-			execution {
-				goals {
-					goal "compile"
-					goal "testCompile"
-				}
-			}
-		}
-	}
+groovy = {plugins ->
+    plugins.plugin {
+        groupId "org.codehaus.groovy.maven"
+        artifactId "gmaven-plugin"
+        version "1.0-rc-2"
+        executions {
+            execution {
+                goals {
+                    goal "compile"
+                    goal "testCompile"
+                }
+            }
+        }
+    }
 }
 
 antlr = {plugins, debugEnabled ->
@@ -103,6 +104,39 @@ antlr = {plugins, debugEnabled ->
                     goal "antlr"
                 }
             }
+        }
+    }
+}
+
+xjc = {executions, name, pkg, schema, dir, clear = "true" ->
+    executions.execution {
+        id name
+        goals {
+            goal "xjc"
+        }
+        configuration {
+            packageName pkg
+            schemaFiles schema
+            staleFile '${project.build.directory}/generated-sources/jaxb/.' + name + 'StaleFlag'
+            clearOutputDir clear
+            schemaDirectory dir
+        }
+    }
+}
+
+war = {plugins, dir ->
+    plugins.plugin {
+        groupId "org.apache.maven.plugins"
+        artifactId "maven-war-plugin"
+        executions {
+            execution {
+                goals {
+                    goal "war"
+                }
+            }
+        }
+        configuration {
+            warSourceDirectory dir
         }
     }
 }

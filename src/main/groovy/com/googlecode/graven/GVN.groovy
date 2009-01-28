e6e1f972c53dfc9120b5e1e0de2df23c953b8047
spullara <spullara@1ddc80c7-8841-0410-a5a5-81f158d74672>
2009-01-28 02:41:41 +0000

    very expensive way to handle modules. have to work on this.

--- src/main/groovy/com/googlecode/graven/GVN.groovy
+++ src/main/groovy/com/googlecode/graven/GVN.groovy
@@ -1,46 +1,64 @@
 package com.googlecode.graven;
 
 import groovy.xml.MarkupBuilder
+import javax.xml.parsers.DocumentBuilderFactory
+import groovy.xml.dom.DOMCategory
 
-// Read the pom.groovy script
-GroovyShell shell = new GroovyShell()
-Script script = shell.parse(new File("pom.groovy"))
 
-// Write out a pom.xml
-def fw = new FileWriter("pom.xml");
+private def createPom(dir = ".") {
+    // Read the pom.groovy script
+    GroovyShell shell = new GroovyShell()
+    Script script = shell.parse(new File(dir, "pom.groovy"))
 
-// Setup the markup builder
-MarkupBuilder mb = new MarkupBuilder(fw)
+    // Write out a pom.xml
+    File pomFile = new File(dir, "pom.xml")
+    def fw = new FileWriter(pomFile);
 
-// Bind the markup builder into the script
-def binding = new Binding()
-binding.setProperty "project", { pom ->
-    mb.project {
-        pom.owner = script
-        pom.delegate = mb
-        pom()
+    // Setup the markup builder
+    MarkupBuilder mb = new MarkupBuilder(fw)
+
+    // Bind the markup builder into the script
+    def binding = new Binding()
+    binding.setProperty "project", {pom ->
+        mb.project {
+            pom.owner = script
+            pom.delegate = mb
+            pom()
+        }
+    }
+
+    // Include all of the variables on the included script in our binding
+    def includeClass = {clazz ->
+        def include = clazz.newInstance()
+        include.run()
+        include.binding.properties.variables.each {
+            binding.setVariable it.key, it.value.curry(mb)
+        }
+    }
+
+    // Standard library
+    includeClass(Pom)
+
+    // Add the includer to the binding
+    binding.setProperty "include", includeClass
+
+    // Set the script binding and execute
+    script.binding = binding
+    def result = script.run();
+    fw.close();
+
+    def builder = DocumentBuilderFactory.newInstance().newDocumentBuilder()
+    def inputStream = new FileInputStream(pomFile)
+    def document = builder.parse(inputStream)
+
+    use(DOMCategory.class) {
+        document.getDocumentElement().modules.module.each {
+            createPom(it.text());
+        }
     }
 }
 
-// Include all of the variables on the included script in our binding
-def includeClass = {clazz ->
-    def include = clazz.newInstance()
-    include.run()
-    include.binding.properties.variables.each {
-        binding.setVariable it.key, it.value.curry(mb)
-    }
-}
-
-// Standard library
-includeClass(Pom)
-
-// Add the includer to the binding
-binding.setProperty "include", includeClass
-
-// Set the script binding and execute
-script.binding = binding
-script.run();
-fw.close();
+createPom();
 
 // Excecute maven 2
 def list = ["mvn"]
